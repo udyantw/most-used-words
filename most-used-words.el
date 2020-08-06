c2158a5b6ceb1f3ef81e16b5b7f13c8aa24787c3
Jen-Chieh <jcs090218@gmail.com>
2020-08-06 15:35:44 +0800

    Add buffer and table display option.

--- most-used-words.el
+++ most-used-words.el
@@ -61,6 +61,18 @@
   :type 'integer
   :group 'most-used-words)
 
+(defcustom most-used-words-display-type 'buffer
+  "The method to display most used words."
+  :type '(choice (const :tag "buffer" buffer)
+                 (const :tag "table" table))
+  :group 'most-used-words)
+
+(defvar most-used-words--buffer-name "*Most used words*"
+  "Buffer name for displaying most used words.")
+
+(defvar most-used-words--buffer nil
+  "Recorded buffer for currently doing most used words.")
+
 ;;; Util
 
 (cl-defmacro most-used-words-with-view-buffer (buffer &body body)
@@ -91,48 +103,92 @@
   "Make a list of the N most used words in buffer.
 
 Optional argument SHOW-PERCENTAGES-P displays word counts and percentages."
-  (let ((counts (make-hash-table :test #'equal))
-        (total-count 0) sorted-counts
-        start end)
-    (save-excursion
-      (goto-char (point-min))
-      (skip-syntax-forward "^w")
-      (setf start (point))
-      (cl-loop until (eobp)
+  (with-current-buffer most-used-words--buffer
+    (let ((counts (make-hash-table :test #'equal))
+          (total-count 0) sorted-counts
+          start end)
+      (save-excursion
+        (goto-char (point-min))
+        (skip-syntax-forward "^w")
+        (setf start (point))
+        (cl-loop until (eobp)
+                 do
+                 (skip-syntax-forward "w")
+                 (setf end (point))
+                 (cl-incf (gethash (buffer-substring start end) counts 0))
+                 (skip-syntax-forward "^w")
+                 (setf start (point))
+                 (cl-incf total-count)))
+      (cl-loop for word being the hash-keys of counts
+               using (hash-values count)
                do
-               (skip-syntax-forward "w")
-               (setf end (point))
-               (cl-incf (gethash (buffer-substring start end) counts 0))
-               (skip-syntax-forward "^w")
-               (setf start (point))
-               (cl-incf total-count)))
-    (cl-loop for word being the hash-keys of counts
-             using (hash-values count)
-             do
-             (push (list word count) sorted-counts)
-             finally (setf sorted-counts (cl-sort sorted-counts #'>
-                                                  :key #'cl-second)))
-    (if show-percentages-p
-        (list (cl-subseq sorted-counts 0 n) total-count)
-      (mapcar #'cl-first (cl-subseq sorted-counts 0 n)))))
+               (push (list word count) sorted-counts)
+               finally (setf sorted-counts (cl-sort sorted-counts #'>
+                                                    :key #'cl-second)))
+      (if show-percentages-p
+          (list (cl-subseq sorted-counts 0 n) total-count)
+        (mapcar #'cl-first (cl-subseq sorted-counts 0 n))))))
 
 (defun most-used-words-buffer-aux (&optional n show-counts-p)
   "Show the N (default 3) most used words in the current buffer.
 
 Optional argument SHOW-COUNTS-P also shows the counts and percentages."
-  (let ((most-used-words-buffer (get-buffer-create "*Most used words*"))
-        (most-used (most-used-words-data n))
-        word count percent)
-    (most-used-words-with-view-buffer
-     most-used-words-buffer
-     (dolist (word-data most-used)
-       (setq word (plist-get word-data :word)
-             count (plist-get word-data :count)
-             percent (plist-get word-data :percent))
-       (if show-counts-p
-           (insert (format "%-24s    %5d    %f%%" word count percent))
-         (insert (format "%s" word)))
-       (newline)))))
+  (let* ((most-used-words--buffer (current-buffer))
+         (most-used-words-buffer (get-buffer-create most-used-words--buffer-name))
+         (most-used (most-used-words-data n))
+         word count percent)
+    (cl-case most-used-words-display-type
+      ('buffer
+       (most-used-words-with-view-buffer
+        most-used-words-buffer
+        (dolist (word-data most-used)
+          (setq word (plist-get word-data :word)
+                count (plist-get word-data :count)
+                percent (plist-get word-data :percent))
+          (if show-counts-p
+              (insert (format "%-24s    %5d    %f%%" word count percent))
+            (insert (format "%s" word)))
+          (newline))))
+      ('table
+       (pop-to-buffer most-used-words--buffer-name)
+       (most-used-words-mode)))))
+
+;;; Tabluldated List
+
+(defconst most-used-words--format
+  (vector (list "Word" 15 t)
+          (list "Counts" 10 t)
+          (list "Percentages" 10 t))
+  "Format to assign to `tabulated-list-format' variable.")
+
+(defun most-used-words--get-entries ()
+  "Data entries for most used words."
+  (let ((most-used (most-used-words-data most-used-words-word-display))
+        (entries '()) (id-count 0))
+    (dolist (word-data most-used)
+      (let ((new-entry '()) (new-entry-value '())
+            (word (plist-get word-data :word))
+            (count (plist-get word-data :count))
+            (percent (plist-get word-data :percent)))
+        (push (concat (format "%s" percent) "%") new-entry-value)  ; Percentages
+        (push (format "%s" count) new-entry-value)  ; Counts
+        (push word new-entry-value)  ; Word
+        (push (vconcat new-entry-value) new-entry)  ; Turn into vector.
+        (push (number-to-string id-count) new-entry)  ; ID
+        (push new-entry entries)
+        (setq id-count (1+ id-count))))
+    entries))
+
+(define-derived-mode most-used-words-mode tabulated-list-mode
+  "most-used-words-mode"
+  "Major mode for Most Used Words display."
+  :group 'most-used-words
+  (setq tabulated-list-format most-used-words--format)
+  (setq tabulated-list-padding 1)
+  (setq tabulated-list-sort-key (cons "Counts" nil))
+  (tabulated-list-init-header)
+  (setq tabulated-list-entries (most-used-words--get-entries))
+  (tabulated-list-print t))
 
 ;;; Entry
 
